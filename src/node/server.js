85587c2ed17eb18dc90e3d3217b00a78e8ef58f8
amni2000 <146669169+Toleas03@users.noreply.github.com>
2024-03-27 00:33:48 +0000

    Setup Heroku deployments 2.3

--- src/node/server.js
+++ src/node/server.js
@@ -1,5 +1,7 @@
 var etherpad = require('etherpad-lite');
 var http = require('http');
 
+console.log('Starting Etherpad...');
+
 var server = http.createServer(etherpad.app);
 server.listen(process.env.PORT || 9001);
